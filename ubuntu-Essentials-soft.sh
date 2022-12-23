97604fce764c4e8fe314e998ab0855a0a81e9eed
sk shahriar ahmed raka <skshahriarahmedraka@gmail.com>
2022-12-23 10:50:26 +0600

    solved

--- ubuntu-Essentials-soft.sh
+++ ubuntu-Essentials-soft.sh
@@ -261,6 +261,7 @@ rm -Rf Orchis-theme
 echo "[-] Download fonts [-]"
 echo "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/DroidSansMono.zip"
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/DroidSansMono.zip
+touch ~/.fonts
 unzip DroidSansMono.zip -d ~/.fonts
 fc-cache -fv
 echo "done!"
