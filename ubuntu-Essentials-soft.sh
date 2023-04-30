4304984d4a66234c9685289fac1f623589ecd645
Sk Shahriar Ahmed Raka <skshahriarahmedraka@gmail.com>
2023-04-30 11:26:16 +0600

    added

--- ubuntu-Essentials-soft.sh
+++ ubuntu-Essentials-soft.sh
@@ -282,6 +282,10 @@ rm ubuntu-gdm-set-background
 
 
 
+#### install flatpak
+sudo apt install flatpak
+sudo apt install gnome-software-plugin-flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo 
 
 
 
